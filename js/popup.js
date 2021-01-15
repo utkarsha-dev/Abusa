51e2f34a1cf695eb47858a014592e350f34f8e1f
Yuvansh <yuvansh101@gmail.com>
2021-01-15 19:42:42 +0530

    jan15

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,4 @@
 $(document).ready(function () {
-  $('#switch').click(function () {
-    $('#flag').prop("disabled", !$("#switch").prop("checked"));
-    $('#delete').prop("disabled", !$("#switch").prop("checked"));
-    $('#Check').prop("disabled", !$("#switch").prop("checked")); 
-    $('#act').prop("disabled", !$("#switch").prop("checked"));
-
-    if (!this.checked) {
-      $("#generate").prop("disabled",true);
-      $('#Check').prop("checked",false);
-      $('#flag').prop("checked",false);
-      $('#delete').prop("checked",false);
-    }
-
-  })
-});
-
-$(document).ready(function () {
   $('#Check').click(function () {
     $('#generate').prop("disabled", !$("#Check").prop("checked")); 
   })
@@ -26,3 +9,18 @@ $(function(){
     alert('Contact Admin!');
   });
 });
+
+
+function words() {
+  var $fl = document.getElementById('flag');
+  var $dl = document.getElementById('delete');
+
+  if($fl.checked){
+      chrome.tabs.executeScript({file: 'scripts/main/flagscript.js'});
+    }
+  else if($dl.checked){
+    chrome.tabs.executeScript({file: 'scripts/main/deletescript.js'});
+  }
+}
+ 
+document.getElementById('act').addEventListener('click', words);
---
+++ scripts/main/deletescript.js
@@ -0,0 +1,47 @@
+alert("NO ABUSE PLEASE ! WE DELETED THE ABUSE WORD")
+arr = []
+id = "abusarandomid";
+idNum = 0;
+var startElem = document.getElementsByTagName('body')[0];
+var items = startElem.getElementsByTagName("*");
+
+for (var i = 0; i < items.length; i++) {
+    if (items[i].childNodes.length === 1)
+    {
+        if (items[i].innerText !== null && items[i].innerText !== "" && items[i].innerText !== undefined && items[i].getAttribute('id') !== 'style' && items[i].getAttribute('id') !== 'script')
+        {
+            if (items[i].getAttribute('id') === null)
+                items[i].setAttribute('id', id + idNum++);
+
+            data = {
+                "id": items[i].getAttribute('id'),
+                "data": items[i].innerText.split(" ")
+            }
+
+            arr.push(data)
+        }
+    }
+}
+
+const url1 = "https://abusa.herokuapp.com/getAbusiveData";
+
+fetch(url1).then(r => {
+    if (r.ok) {
+        printData(r.json());
+    }
+});
+
+function printData(data) {
+    data.then(r => {
+        checkData(r);
+    });
+}
+
+function checkData(data) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].data.length; j++) {
+            if (arr[i].data[j].toLowerCase() in data && document.getElementById(arr[i].id).firstChild.nodeValue !== null)
+                document.getElementById(arr[i].id).firstChild.nodeValue = document.getElementById(arr[i].id).firstChild.nodeValue.replace(arr[i].data[j], "*****");
+        }
+    }
+}

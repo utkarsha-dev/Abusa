2e2fe9d6c14b75dd06d5a6b1a7e6220ba979e0e0
Yuvansh <yuvansh101@gmail.com>
2021-01-28 00:44:18 +0530

    bottomchange

--- js/bottom.js
+++ js/bottom.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(event){
-    var dataText = [ "Say No To Abuse", "Stop Bullying", "Talk politely", "Respect Everyone!"];
+    var dataText = [ "Don't Abuse", "Stop Bullying", "Talk politely", "Respect Everyone!"];
     
     function typeWriter(text, i, fnCallback) {
       if (i < (text.length)) {

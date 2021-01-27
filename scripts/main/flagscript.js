857e73fc4068fdcd1b33db8dea290bc585780f81
Arushi Jain <58386588+Arushi0302@users.noreply.github.com>
2021-01-27 22:40:51 +0530

    Update flagscript.js

--- scripts/main/flagscript.js
+++ scripts/main/flagscript.js
@@ -1,4 +1,5 @@
-alert("NO ABUSE PLEASE ! WE FLAGGED THE ABUSE WORD")
+alert("NO ABUSE PLEASE ! Abusa have flagged the abusive words and Highlighted the content.")
+alert("WE GOTTA PROBLEM HERE DUDE!!?? NO ABUSE PLZZ...")
 arr = []
 id = "abusarandomid";
 idNum = 0;
@@ -23,9 +24,7 @@ for (var i = 0; i < items.length; i++) {
     }
 }
 
-const url = "https://abusa.herokuapp.com/getAbusiveData";
-
-fetch(url).then(r => {
+fetch("https://abusa.herokuapp.com/getAbusiveData").then(r => {
     if (r.ok) {
         printData(r.json());
     }
@@ -45,3 +44,75 @@ function checkData(data) {
         }
     }
 }
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: JSON.stringify(data)
+    });
+
+    return response.json();
+}
+
+data_value = '';
+var intervalID;
+
+function final_data(data = []) {
+    console.log(data);
+    //Play With Your Data
+    for (var i = 0; i < data.length; i++)
+    {
+        max_key = 'Hate';
+        for (const [key, value] of Object.entries(data[i].predictions)) {
+            max_key = data[i].predictions[max_key] > value? max_key: (key == 'Toxic' && data[i].predictions[max_key] > 0.6? max_key: key);
+        }
+
+        switch (max_key) {
+            case 'Hate':
+                document.getElementById(data[i].id).setAttribute('style', 'color: purple');
+                break;
+            case 'Insult':
+                document.getElementById(data[i].id).setAttribute('style', 'color: lightblue');
+                break;
+            case 'Obscene':
+                document.getElementById(data[i].id).setAttribute('style', 'color: red');
+                break;
+            case 'Threat':
+                document.getElementById(data[i].id).setAttribute('style', 'color: brown');
+                break;
+            default:
+                document.getElementById(data[i].id).setAttribute('style', 'color: yellow');
+                break;
+        }
+    }
+}
+
+function responce_handeler(res) {
+    res.then(data => {
+        console.log(data);
+        if (data.response === "success") {
+            clearInterval(intervalID);
+            final_data(data.data);
+        }
+    });
+}
+
+async function getData(url = '') {
+    const response = await fetch(url);
+
+    responce_handeler(response.json());
+}
+
+function ticData() {
+    intervalID = setInterval(getData, 10000, 'https://abusa.herokuapp.com/getModelOutput' + '?threadnum=' + data_value)
+}
+
+postData("https://abusa.herokuapp.com/modelData", arr)
+    .then(data => {
+        console.log(data);
+        data_value = data.threadnum;
+        ticData();
+    });
